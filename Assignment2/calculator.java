4e874bcff56129c62d6812e4cf29df4b0868f85c
Earl Anthony Gamboa <111488396+Tsukkei@users.noreply.github.com>
2022-09-15 16:25:10 +0800

    Create calculator.java

---
+++ Assignment2/calculator.java
@@ -0,0 +1,43 @@
+// import the Scanner class
+import java.util.Scanner;
+
+public class App {
+
+    // Scanner Object
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // Method call
+        int product = multiply();
+        System.out.println("The product is: " + product);
+
+        // Method call
+        int quotient = divide();
+        System.out.println("The quotient is: " + quotient);
+
+        scan.close();
+
+    } 
+
+    static int multiply() {
+        System.out.println("Please enter two numbers to multiply");
+        System.out.print("Enter first number: ");
+        int x = scan.nextInt();
+
+        System.out.print("Enter second number: ");
+        int y = scan.nextInt();
+        return x * y;
+    } 
+
+    static int divide() {
+        System.out.println("Please enter two numbers to divide");
+        System.out.print("Enter first number: ");
+        int x = scan.nextInt();
+
+        System.out.print("Enter second number: ");
+        int y = scan.nextInt();
+        return x / y;
+    }
+
+}    
